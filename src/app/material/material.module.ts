466b4da3937bd3a762373c7363a598ecf1fcdcb1
Mike Ambrozaitis <mambroz27@users.noreply.github.com>
2021-02-25 09:39:34 -0500

    Set up Material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+const MaterialComponents: any[] = [
+];
+
+
+
+@NgModule({
+  imports: MaterialComponents,
+  exports: MaterialComponents
+})
+export class MaterialModule { }
