6ea0d07a0b6ba44d118282491efb6c65141ddbe4
Mike Ambrozaitis <mambroz27@users.noreply.github.com>
2021-02-26 15:35:22 -0500

    Updated dark mode transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { DarkModeService } from './shared/services/dark-mode.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private unsubscribe: Subject<void> = new Subject();
+
+  darkMode: boolean = false;
+
+  constructor(
+    private darkModeService: DarkModeService
+  ) {}
+
+  ngOnInit(): void {
+    this.darkModeService.on$
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(d => this.darkMode = d);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 }
