1a547b09c7c00f06692e061e46a138e973f438a3
Mike Ambrozaitis <mambroz27@users.noreply.github.com>
2021-02-27 19:38:02 -0500

    Saving dark mode setting to local storage

--- src/app/shared/services/dark-mode.service.ts
+++ src/app/shared/services/dark-mode.service.ts
@@ -7,7 +7,26 @@ import { BehaviorSubject } from 'rxjs';
 export class DarkModeService {
   on$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
+  constructor() {
+    this.init();
+  }
+
+  /**
+   * Check if setting is in local storage
+   */
+  init() {
+    const saved = localStorage.getItem('dark-mode');
+
+    if (saved !== null) {
+      this.on$.next(JSON.parse(saved));
+    }
+  }
+
+  /**
+   * Toggle dark mode
+   */
   toggle() {
     this.on$.next(!this.on$.value);
+    localStorage.setItem('dark-mode', JSON.stringify(this.on$.value));
   }
 }
